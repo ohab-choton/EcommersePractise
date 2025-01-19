a1d0271a6aeb2567a2a6ed52e4ac2cf9938d44d9
Abdul Ohab <ohabchoton@gmail.com>
2025-01-20 00:38:34 +0600

    model created

--- Eshop/main/admin.py
+++ Eshop/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category,Brand,Color,Size
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Size)
--- Eshop/main/models.py
+++ Eshop/main/models.py
@@ -22,3 +22,22 @@ class Size(models.Model):
     title=models.CharField(max_length=100)
     def __str__(self):
         return self.title
+    
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='products/' )
+    slug=models.SlugField(max_length=100)
+    details=models.TextField()
+    spec=models.TextField()
+    price=models.PositiveIntegerField()
+    brand=models.ForeignKey(Brand,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    color=models.ForeignKey(Color,on_delete=models.CASCADE)
+    size=models.ForeignKey(Size,on_delete=models.CASCADE)
+    status=models.BooleanField(default=True)
+    def __str__(self):
+        return self.title
+    
+
+    
+    
